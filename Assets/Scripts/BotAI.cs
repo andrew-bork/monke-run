f43128c18ccffbb966af1211ea92d611ece6c23e
NhaleBoy <104642921+NhaleBoy@users.noreply.github.com>
2024-04-26 14:55:52 -0700

    Updated Bot
    Added death state and jumping to bot

--- Assets/Scripts/BotAI.cs
+++ Assets/Scripts/BotAI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class BotAI : MonoBehaviour
 {
@@ -11,15 +12,31 @@ public class BotAI : MonoBehaviour
 
     Vector3 dest;
 
-    public float speed;
+    public Camera jumpScareCam;
 
+    public float speed, catchDistance, jumpScareTime;
+
+    public string sceneAfterDeath;
    
 
 
     void Update()
     {
+        float distance = Vector3.Distance(transform.position, player.position);
         agent.speed = speed;
         dest = player.position;
         agent.destination = dest;
+        if(distance <= catchDistance)
+        {
+            player.gameObject.SetActive(false);
+            jumpScareCam.gameObject.SetActive(true);
+            StartCoroutine(killPlayer());
+        }
+    }
+
+    IEnumerator killPlayer()
+    {
+        yield return new WaitForSeconds(jumpScareTime);
+        SceneManager.LoadScene(sceneAfterDeath);
     }
 }
