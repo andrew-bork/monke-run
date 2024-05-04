4f3d0c3da1747cf146d644e2644aeb9e7e7f8ff0
NhaleBoy <104642921+NhaleBoy@users.noreply.github.com>
2024-05-04 12:27:21 -0700

    Jump debugging
    jump no worky

--- Assets/Scripts/BotAI.cs
+++ Assets/Scripts/BotAI.cs
@@ -19,9 +19,13 @@ public class BotAI : MonoBehaviour
     public string sceneAfterDeath;
 
     public Rigidbody rb;
-   
+
+    public bool isGrounded;
+
     private void Start()
     {
+        isGrounded = true;
+        agent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -31,13 +35,28 @@ public class BotAI : MonoBehaviour
         agent.speed = speed;
         dest = player.position;
         agent.destination = dest;
-        rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
-        if (!agent.hasPath)
+        if (!agent.hasPath && isGrounded)
         {
-            Debug.Log("no path");
+            Debug.Log("ready to jump");
+            if (agent.enabled)
+            {
+                // set the agents target to where you are before the jump
+                // this stops her before she jumps. Alternatively, you could
+                // cache this value, and set it again once the jump is complete
+                // to continue the original move
+                // disable the agent
+                agent.updatePosition = false;
+                agent.updateRotation = false;
+                agent.isStopped = true;
+            }
+            // make the jump
+            rb.isKinematic = false;
+            rb.useGravity = true;
             
+            isGrounded = false;
+            Debug.Log("Jumped");
         }
-        if(distance <= catchDistance)
+        if (distance <= catchDistance)
         {
             player.gameObject.SetActive(false);
             jumpScareCam.gameObject.SetActive(true);
@@ -50,4 +69,27 @@ public class BotAI : MonoBehaviour
         yield return new WaitForSeconds(jumpScareTime);
         SceneManager.LoadScene(sceneAfterDeath);
     }
+
+   private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider != null && collision.collider.tag == "Floor")
+        {
+            if (!isGrounded)
+            {
+                if (agent.enabled)
+                {
+                    agent.updatePosition = true;
+                    agent.updateRotation = true;
+                    agent.isStopped = false;
+                }
+                rb.isKinematic = true;
+                rb.useGravity = false;
+                isGrounded = true;
+                agent.destination = dest;
+
+                Debug.Log("agent enabled");
+            }
+        }
+    }
+   
 }
